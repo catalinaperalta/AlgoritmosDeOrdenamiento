b28477148b360da2bd08cc67cb661f1bda3526b5
catalinaperalta <catalinaperaltah@hotmail.com>
2015-09-30 19:41:50 -0500

    Update Rojinegro.h

--- Rojinegro.h
+++ Rojinegro.h
@@ -23,6 +23,8 @@ public:
     NodoB<T> * uncle(NodoB<T> * nodo);
     NodoB<T> * sibling(NodoB<T> * nodo);
     
+    NodoB<T> * maximum_node(NodoB<T> * nodo);
+    
     void verifyProperties(Rojinegro * arbol);
     void verifyProperty1(NodoB<T> * nodo);
     void verifyProperty2(NodoB<T> * nodo);
@@ -48,8 +50,11 @@ public:
     void deleteCase3(Rojinegro * arbol, NodoB<T> * nodo);
     void deleteCase4(Rojinegro * arbol, NodoB<T> * nodo);
     void deleteCase5(Rojinegro * arbol, NodoB<T> * nodo);
+    void deleteCase6(Rojinegro * arbol, NodoB<T> * nodo);
     
     void prettyPrint(NodoB<T>* p, int indent);
+    
+    void verificarArbol(Rojinegro<T> * arbol);
 };
 
 template<class T>
@@ -87,6 +92,19 @@ NodoB<T> * Rojinegro<T>::sibling(NodoB<T> * nodo)
         else
             return nodo->getPadre()->getIzquierdo();
     }
+    else
+        return nullptr;
+}
+
+template<class T>
+NodoB<T> * Rojinegro<T>::maximum_node(NodoB<T> * nodo)
+{
+    assert(nodo != nullptr);
+    while(nodo->getDerecho() != nullptr)
+    {
+        nodo = nodo->getDerecho();
+    }
+    return nodo;
 }
 
 template<class T>
@@ -136,35 +154,50 @@ void Rojinegro<T>::verifyProperty3(NodoB<T> * nodo)
 template <class T>
 void Rojinegro<T>::verifyProperty4(NodoB<T> * root)
 {
+    
     int blackPath = -1;
-    verifyProperty4Helper(root, 0, blackPath);
+    std::string mensaje="";
+    
+    return verifyProperty4Helper(root, 0, blackPath);
 }
 
 template <class T>
 void Rojinegro<T>::verifyProperty4Helper(NodoB<T> * nodo, int contNegros, int& blackPath)
 {
+    std::string color;
+    
     
     if (nodo != nullptr) {
-        if (nodo->getColor() == "negro")
-        {
-            contNegros++;
-        }
-        if (nodo == nullptr)
-        {
-            if (blackPath == -1)
-            {
-                blackPath = contNegros;
-            }
-            else
-            {
-                assert (contNegros == blackPath);
-            }
-            return;
-        }
-        verifyProperty4Helper(nodo->getIzquierdo(),  contNegros, blackPath);
-        verifyProperty4Helper(nodo->getDerecho(), contNegros, blackPath);
+        color = nodo->getColor();
+    }
+    else{
+        color = "";
     }
     
+    if (color == "negro")
+    {
+        contNegros++;
+    }
+    if (nodo == nullptr)
+    {
+        if (blackPath == -1)
+        {
+            blackPath = contNegros;
+        }
+        else
+        {
+            //assert (contNegros == blackPath);
+            if (contNegros != blackPath)
+            {
+                std::cout << "No es Rojinegro" << std::endl;
+            }
+        }
+        return;
+    }
+    verifyProperty4Helper(nodo->getIzquierdo(),  contNegros, blackPath);
+    verifyProperty4Helper(nodo->getDerecho(), contNegros, blackPath);
+    
+    return;
 }
 
 template<class T>
@@ -177,104 +210,118 @@ std::string Rojinegro<T>::color(NodoB<T> * nodo)
 }
 
 template<class T>
-void Rojinegro<T>::rotateLeft(Rojinegro * arbol, NodoB<T> * node_x)
+void Rojinegro<T>::rotateLeft(Rojinegro * arbol, NodoB<T> * nodoX)
 {
-    NodoB<T> *node_y;
+    NodoB<T> * nodoY;
     
-    if(node_x->getDerecho() == NULL){
+    if(nodoX->getDerecho() == nullptr)
+    {
         return;
     }
     
-    node_y = node_x->getDerecho();
+    nodoY = nodoX->getDerecho();
     
-    if(node_y->getIzquierdo() != NULL){
-        node_y->getIzquierdo()->setPadre(node_x);
-        node_x->setDerecho(node_y->getIzquierdo());
+    if(nodoY->getIzquierdo() != nullptr)
+    {
+        nodoY->getIzquierdo()->setPadre(nodoX);
+        nodoX->setDerecho(nodoY->getIzquierdo());
     }
     
-    node_y->setPadre(node_x->getPadre());
+    nodoY->setPadre(nodoX->getPadre());
     
-    if(node_x->getPadre() == NULL){
-        this->setRaiz(node_y);
-    }else if(node_x == node_x->getPadre()->getIzquierdo()){
-        node_x->getPadre()->setIzquierdo(node_y);
-    }else{
-        node_x->getPadre()->setDerecho(node_y);
+    if(nodoX->getPadre() == nullptr)
+    {
+        this->setRaiz(nodoY);
+    }
+    else if(nodoX == nodoX->getPadre()->getIzquierdo())
+    {
+        nodoX->getPadre()->setIzquierdo(nodoY);
+    }
+    else
+    {
+        nodoX->getPadre()->setDerecho(nodoY);
     }
     
-    node_x->setDerecho(node_y->getIzquierdo());
-    node_y->setIzquierdo(node_x);
-    node_x->setPadre(node_y);//    NodoB<T> * temp = nodo->getDerecho();
-//    
-//    if(nodo->getPadre() == nullptr)
-//        arbol->setRaiz(temp);
-//    else if(nodo->getPadre()->getIzquierdo() == nodo)
-//        nodo->getPadre()->setIzquierdo(temp);
-//    else
-//        nodo->getPadre()->setDerecho(temp);
-//
-//    if(temp != nullptr)
-//        temp->setPadre(nodo->getPadre());
-//    
-//    nodo->setDerecho(temp->getIzquierdo());
-//    
-//    if (temp->getIzquierdo() != nullptr)
-//    {
-//        temp->getIzquierdo()->setPadre(nodo);
-//    }
-//    temp->setIzquierdo(nodo);
-//    nodo->setPadre(temp);
+    nodoX->setDerecho(nodoY->getIzquierdo());
+    nodoY->setIzquierdo(nodoX);
+    nodoX->setPadre(nodoY);//    NodoB<T> * temp = nodo->getDerecho();
+    //
+    //    if(nodo->getPadre() == nullptr)
+    //        arbol->setRaiz(temp);
+    //    else if(nodo->getPadre()->getIzquierdo() == nodo)
+    //        nodo->getPadre()->setIzquierdo(temp);
+    //    else
+    //        nodo->getPadre()->setDerecho(temp);
+    //
+    //    if(temp != nullptr)
+    //        temp->setPadre(nodo->getPadre());
+    //
+    //    nodo->setDerecho(temp->getIzquierdo());
+    //
+    //    if (temp->getIzquierdo() != nullptr)
+    //    {
+    //        temp->getIzquierdo()->setPadre(nodo);
+    //    }
+    //    temp->setIzquierdo(nodo);
+    //    nodo->setPadre(temp);
 }
 
 template<class T>
-void Rojinegro<T>::rotateRight(Rojinegro * arbol, NodoB<T> * node_y)
+void Rojinegro<T>::rotateRight(Rojinegro * arbol, NodoB<T> * nodoY)
 {
-    NodoB<T> *node_x;
+    NodoB<T> *nodoX;
     
-    if(node_y->getIzquierdo() == NULL){
+    if(nodoY->getIzquierdo() == nullptr)
+    {
         return;
     }
     
-    node_x = node_y->getIzquierdo();
+    nodoX = nodoY->getIzquierdo();
     
-    if(node_x->getDerecho() != NULL){
-        node_x->getDerecho()->setPadre(node_y);
-        node_y->setIzquierdo(node_x->getDerecho());
+    if(nodoX->getDerecho() != nullptr)
+    {
+        nodoX->getDerecho()->setPadre(nodoY);
+        nodoY->setIzquierdo(nodoX->getDerecho());
     }
     
-    node_x->setPadre(node_y->getPadre());
+    nodoX->setPadre(nodoY->getPadre());
     
-    if(node_y->getPadre() == NULL){
-        this->setRaiz(node_x);
-    }else if(node_y == node_y->getPadre()->getIzquierdo()){
-        node_y->getPadre()->setIzquierdo(node_x);
-    }else{
-        node_y->getPadre()->setDerecho(node_x);
+    if(nodoY->getPadre() == nullptr)
+    {
+        this->setRaiz(nodoX);
+    }
+    else if(nodoY == nodoY->getPadre()->getIzquierdo())
+    {
+        nodoY->getPadre()->setIzquierdo(nodoX);
+    }
+    else
+    {
+        nodoY->getPadre()->setDerecho(nodoX);
     }
     
-    node_y->setIzquierdo(node_x->getDerecho());
-    node_x->setDerecho(node_y);
-    node_y->setPadre(node_x);
-
-//    NodoB<T> * temp = nodo->getIzquierdo();
-//    
-//    if(nodo->getPadre() == nullptr)
-//        arbol->setRaiz(temp);
-//    else if(nodo->getPadre()->getIzquierdo() == nodo)
-//        nodo->getPadre()->setIzquierdo(temp);
-//    else
-//        nodo->getPadre()->setDerecho(temp);
-//    
-//    if(temp != nullptr)
-//        temp->setPadre(nodo->getPadre());
-//    
-//    nodo->setIzquierdo(temp->getDerecho());
-//    if (temp->getDerecho() != nullptr)
-//    {
-//        nodo->getDerecho()->setPadre(nodo);
-//    }
-//    temp->setDerecho(nodo);
-//    nodo->setPadre(temp);
+    nodoY->setIzquierdo(nodoX->getDerecho());
+    nodoX->setDerecho(nodoY);
+    nodoY->setPadre(nodoX);
+    
+    //    NodoB<T> * temp = nodo->getIzquierdo();
+    //
+    //    if(nodo->getPadre() == nullptr)
+    //        arbol->setRaiz(temp);
+    //    else if(nodo->getPadre()->getIzquierdo() == nodo)
+    //        nodo->getPadre()->setIzquierdo(temp);
+    //    else
+    //        nodo->getPadre()->setDerecho(temp);
+    //
+    //    if(temp != nullptr)
+    //        temp->setPadre(nodo->getPadre());
+    //
+    //    nodo->setIzquierdo(temp->getDerecho());
+    //    if (temp->getDerecho() != nullptr)
+    //    {
+    //        nodo->getDerecho()->setPadre(nodo);
+    //    }
+    //    temp->setDerecho(nodo);
+    //    nodo->setPadre(temp);
 }
 
 template<class T>
@@ -356,58 +403,133 @@ void Rojinegro<T>::insertCase5(Rojinegro * arbol, NodoB<T> * nodo)
 template <class T>
 void Rojinegro<T>::RBdelete(T valor)
 {
-//    NodoB<T> * hijo;
-//    NodoB<T> * nodo;
-//    nodo = this->buscar(valor);
-//    if (nodo == nullptr)
-//        return;
-//    
-//    if (nodo->getIzquierdo() != nullptr && nodo->getDerecho() != nullptr)
-//    {
-//        NodoB<T> * pred = maximum_node(nodo->getIzquierdo());
-//        nodo->getInfo() = pred->getInfo();
-//        nodo = pred;
-//    }
-//    assert(nodo->getIzquierdo() == NULL || nodo->getDerecho() == nullptr);
-//    hijo = nodo->getDerecho() == nullptr ? nodo->getIzquierdo()  : nodo->getDerecho();
-//    if (color(nodo) == "negro")
-//    {
-//        nodo->getColor() = color(hijo);
-//        //deleteCase1(this, nodo);
-//    }
-//    replace_node(this, nodo, hijo);
-//    free(nodo);
-//    verifyProperties(this);
+    NodoB<T> * hijo;
+    NodoB<T> * nodo;
+    nodo = this->buscar(valor);
+    if (nodo == nullptr)
+        return;
+    
+    if (nodo->getIzquierdo() != nullptr && nodo->getDerecho() != nullptr)
+    {
+        NodoB<T> * pred = maximum_node(nodo->getIzquierdo());
+        nodo->setInfo(pred->getInfo());
+        nodo = pred;
+    }
+    assert(nodo->getIzquierdo() == NULL || nodo->getDerecho() == nullptr);
+    hijo = nodo->getDerecho() == nullptr ? nodo->getIzquierdo()  : nodo->getDerecho();
+    if (color(nodo) == "negro")
+    {
+        nodo->getColor() = color(hijo);
+        deleteCase1(this, nodo);
+    }
+    
+    
+    if (nodo->getPadre() == nullptr)
+    {
+        this->setRaiz(hijo);
+    }
+    else
+    {
+        if (nodo == nodo->getPadre()->getIzquierdo())
+            nodo->getPadre()->setIzquierdo(hijo);
+        else
+            nodo->getPadre()->setDerecho(hijo);
+    }
+    if (hijo != nullptr)
+    {
+        hijo->setPadre(nodo->getPadre());
+    }
+    delete nodo;
+    verifyProperties(this);
 }
 
 template <class T>
 void Rojinegro<T>::deleteCase1(Rojinegro * arbol, NodoB<T> * nodo)
 {
-    
+    if(nodo->getPadre() == nullptr)
+        return;
+    else
+        deleteCase2(this, nodo);
 }
 
 template <class T>
 void Rojinegro<T>::deleteCase2(Rojinegro * arbol, NodoB<T> * nodo)
 {
-    
+    if (color(sibling(nodo)) == "rojo")
+    {
+        nodo->getPadre()->setColor("rojo");
+        sibling(nodo)->setColor("negro");
+        if (nodo == nodo->getPadre()->getIzquierdo())
+            rotateLeft(this, nodo->getPadre());
+        else
+            rotateRight(this, nodo->getPadre());
+    }
+    deleteCase3(this, nodo);
 }
 
 template <class T>
 void Rojinegro<T>::deleteCase3(Rojinegro * arbol, NodoB<T> * nodo)
 {
-    
+    if (color(nodo->getPadre()) == "negro" && color(sibling(nodo)) == "negro" &&
+        color(sibling(nodo)->getIzquierdo()) == "negro" && color(sibling(nodo)->getDerecho()) == "negro")
+    {
+        sibling(nodo)->setColor("rojo");
+        deleteCase1(this, nodo->getPadre());
+    }
+    else
+        deleteCase4(this, nodo);
 }
 
 template <class T>
 void Rojinegro<T>::deleteCase4(Rojinegro * arbol, NodoB<T> * nodo)
 {
-    
+    if (color(nodo->getPadre()) == "rojo" && color(sibling(nodo)) == "negro" &&
+        color(sibling(nodo)->getIzquierdo()) == "negro" && color(sibling(nodo)->getDerecho()) == "negro")
+    {
+        sibling(nodo)->setColor("rojo");
+        nodo->getPadre()->setColor("negro");
+    }
+    else
+        deleteCase5(this, nodo);
 }
 
 template<class T>
 void Rojinegro<T>::deleteCase5(Rojinegro * arbol, NodoB<T> * nodo)
 {
-    
+    if (nodo == nodo->getPadre()->getIzquierdo() && color(sibling(nodo)) == "negro" &&
+        color(sibling(nodo)->getIzquierdo()) == "rojo" && color(sibling(nodo)->getDerecho()) == "negro")
+    {
+        sibling(nodo)->setColor("rojo");
+        sibling(nodo)->getIzquierdo()->setColor("negro");
+        rotateRight(this, sibling(nodo));
+    }
+    else if (nodo == nodo->getPadre()->getDerecho() && color(sibling(nodo)) == "negro" &&
+             color(sibling(nodo)->getDerecho()) == "rojo" && color(sibling(nodo)->getIzquierdo()) == "negro")
+    {
+        sibling(nodo)->setColor("rojo");
+        sibling(nodo)->getDerecho()->setColor("negro");
+        rotateLeft(this, sibling(nodo));
+    }
+    deleteCase6(this, nodo);
+}
+
+template<class T>
+void Rojinegro<T>::deleteCase6(Rojinegro * arbol, NodoB<T> * nodo)
+{
+    sibling(nodo)->setColor(color(nodo->getPadre()));
+    nodo->getPadre()->setColor("negro");
+    if (nodo == nodo->getPadre()->getIzquierdo())
+    {
+        assert(color(sibling(nodo)->getDerecho()) == "rojo");
+        sibling(nodo)->getDerecho()->setColor("negro");
+        rotateLeft(this, nodo->getPadre());
+    }
+    else
+    {
+        assert(color(sibling(nodo)->getIzquierdo()) == "rojo");
+        sibling(nodo)->getIzquierdo()->setColor("negro");
+        rotateRight(this, nodo->getPadre());
+    }
 }
 
 template <class T>
@@ -427,6 +549,12 @@ void Rojinegro<T>::prettyPrint(NodoB<T>* p, int indent)
             prettyPrint(p->getIzquierdo(), indent + 4);
         }
     }
+}
+
+template<class T>
+void Rojinegro<T>::verificarArbol(Rojinegro<T> * arbol)
+{
+    verifyProperties(arbol);
 }
 
 #endif
