d18edd910d07f5c822fe39cdef8d9e6f6f953c0d
catalinaperalta <catalinaperaltah@hotmail.com>
2015-10-01 23:59:17 -0500

    Update ArbolBinarioBusqueda.h

--- ArbolBinarioBusqueda.h
+++ ArbolBinarioBusqueda.h
@@ -19,7 +19,18 @@ class ArbolBinarioBusqueda : public ArbolBinario<T>
 public:
     ArbolBinarioBusqueda();
     virtual NodoB<T>* insertar(T valor);
-    void removeNodo(T valor);
+    NodoB<T> * removeNodo(NodoB<T> * nodo, T valor);
+    
+    NodoB<T> * minNodo(NodoB<T> * nodo)
+    {
+        NodoB<T> * actual = nodo;
+        
+        while (actual->getIzquierdo() != nullptr)
+            actual = actual->getIzquierdo();
+        
+        return actual;
+    }
+    
     void print();
 };
 
@@ -79,113 +90,6 @@ NodoB<T>* ArbolBinarioBusqueda<T>::insertar(T valor)
     return nodo;
     
     
-}
-
-template<class T>
-void ArbolBinarioBusqueda<T>::removeNodo(T valor)
-{
-    NodoB<T> * root = this->getRaiz();
-    
-    if(this->empty())
-    {
-        return;
-    }
-    
-    NodoB<T> * actual = root;
-    NodoB<T> * parent;
-    
-    // root to leaf search (top-down)
-    while(actual)
-    {
-        if(actual->getInfo() == valor)
-        {
-            break;
-        }
-        else
-        {
-            parent = actual;
-            actual = valor > actual->getInfo() ? actual->getDerecho() : actual->getIzquierdo();
-        }
-    }
-    
-    if(actual == nullptr)
-    {
-        return;
-    }
-    
-    
-    // 3 cases :
-    // 1. We're removing a leaf node
-    // 2. We're removing a node with a single child
-    // 3. we're removing a node with 2 children
-    
-    
-    //We're looking at a leaf node
-    if( actual->getIzquierdo() == nullptr and actual->getDerecho() == nullptr) {
-        if(parent->getIzquierdo() == actual)
-            parent->setIzquierdo(nullptr);
-        else
-            parent->setDerecho(nullptr);
-        
-        delete actual;
-       
-        return;
-    }
-    
-    // Node with single child
-    if((actual->getIzquierdo() == nullptr and actual->getDerecho() != nullptr) or (actual->getIzquierdo() != nullptr and actual->getDerecho() == nullptr)) {
-        if(actual->getIzquierdo() == nullptr and actual->getDerecho() != nullptr) {
-            if(parent->getIzquierdo() == actual) {
-                parent->setIzquierdo(actual->getDerecho());
-            } else {
-                parent->setDerecho(actual->getDerecho());
-            }
-        } else { // getIzquierdo child present, no right child
-            if(parent->getIzquierdo() == actual) {
-                parent->setIzquierdo(actual->getIzquierdo());
-            } else {
-                parent->setDerecho(actual->getIzquierdo());
-            }
-        }
-        delete actual;
-        
-        return;
-    }
-    
-    // Node with 2 children
-    // replace node with smallest value in right subtree
-    if (actual->getIzquierdo() != nullptr and actual->getDerecho() != nullptr) {
-        NodoB<T> *actual_right = actual->getDerecho();
-        if(actual_right->getIzquierdo() == nullptr and actual_right->getDerecho() == nullptr) {
-            actual->setInfo(actual_right->getInfo());
-            delete actual_right;
-            actual->setDerecho(nullptr);
-        } else { // right child has children
-            //if the node's right child has a getIzquierdo child
-            // Move all the way down getIzquierdo to locate smallest element
-            
-            if((actual->getDerecho())->getIzquierdo() != nullptr) {
-                NodoB<T>* lactual;
-                NodoB<T>* lactual_parent;
-                lactual_parent = actual->getDerecho();
-                lactual = (actual->getDerecho())->getIzquierdo();
-                while(lactual->getIzquierdo() != nullptr) {
-                    lactual_parent = lactual;
-                    lactual = lactual->getIzquierdo();
-                }
-                actual->setInfo(lactual->getInfo());
-                delete lactual;
-                lactual_parent->setIzquierdo(nullptr);
-            } else { // (actual->right)->right != nullptr
-                NodoB<T> *tmp = actual->getDerecho();
-                actual->setInfo(tmp->getInfo());
-                actual->setDerecho(tmp->getDerecho());
-                delete tmp;
-            }
-        }
-        
-    }
-
 }
 
 template<class T>
